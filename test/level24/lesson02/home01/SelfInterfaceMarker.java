cd3c1cc8476adfa54422d29de9bbad70dee0abf3
nikita <n.tyuftin@yandex.ru>
2016-12-26 23:06:37 +0300

    26.12.2016

---
+++ test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by IEUser on 25.12.2016.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by IEUser on 25.12.2016.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public SelfInterfaceMarkerImpl() {
+    }
+
+    public void print() {
+        System.out.println();
+    }
+}
